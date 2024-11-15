fe96f13bfc2d151d7fc5d914b94bd9d884c47b6b
Anderson Campos <anderson.camposs87@outlook.com>
2024-11-15 10:06:21 -0300

    Options API e CompositionAPI

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import { ref, onMounted, onUpdated } from 'vue';
+
+    const count = ref(0);
+
+    function increment() {
+        count.value++;
+    }
+
+    onMounted(() => {
+        console.log("CompositionAPI\nO componente foi montado");
+    })
+
+    onUpdated(() => {
+        console.log(`CompositionAPI\nO componente foi Re-Renderizado\nValor do contador: ${count.value}`)
+    })
+
+</script>
+
+<template>
+    <h1>Composition API</h1>
+    <h3>{{ count }}</h3>
+    <button v-on:click="increment">Incrementar</button>
+</template>
---
+++ src/components/OptionsAPI.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+
+    methods: {
+        increment() {
+            this.count++
+        }
+    },
+
+    mounted() {
+        console.log(`OptionsAPI\nO componente foi montado`);
+    }, 
+
+    updated() {
+        console.log(`OptionsAPI\nComponente Re-Renderizado\nValor do contador: ${this.count}`)
+    }
+}
+</script>
+
+<template>
+    <h1>Options API</h1>
+    <h3>{{ count }}</h3>
+    <button v-on:click="increment">Incrementar</button>
+</template>
