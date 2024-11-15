def794edb9fafa4a7a3e6d59085124699fff7d06
AndersonCamposs <anderson.camposs87@outlook.com>
2024-11-15 11:38:52 -0300

    Dados reativos com ref e reactive

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import CompositionAPI from "./components/CompositionAPI.vue";
 import OptionsAPI from "./components/OptionsAPI.vue"
+import ReactiveData from "./components/ReactiveData.vue";
 </script>
 
 <template>
   <OptionsAPI />
   <hr>
   <CompositionAPI />
+  <hr>
+  <ReactiveData />
 </template>
 
 <style scoped>
---
+++ src/components/ReactiveData.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref, reactive } from 'vue';
+const refCount = ref(0);
+const object = reactive({
+    count: 0
+})
+
+function incrementRef() {
+    refCount.value++;
+}
+
+function incrementReactive() {
+    object.count++
+}
+</script>
+
+<template>
+<div class="container">
+    <div>
+        <h1>Usando Ref Object</h1>
+        <button @click="incrementRef">Valor do Ref: {{ refCount }}</button>
+    </div>
+    <div>
+        <h1>Usando Reactive Object</h1>
+        <button @click="incrementReactive">Valor do object.count: {{ object.count }}</button>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+}
+
+.container div {
+    border-radius: 8px;
+    border: 2px solid #000;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 10px;
+    margin: 0 10px;
+}
+</style>
