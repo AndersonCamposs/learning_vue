f8879d0264f1b83671bfe7c046ef88d795ac6f0c
Anderson Campos <anderson.camposs87@outlook.com>
2024-11-15 22:01:47 -0300

    Vinculação de classe

---
+++ src/components/ClassBinding.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+    const hasError = ref(false);
+
+    const setClass = () => {
+        hasError.value = !hasError.value;
+    }
+</script>
+
+<template>
+    <h1>Ligação de classe</h1>
+    <h3 v-bind:class="{'text-danger': hasError}">Texto de exemplo</h3>
+    <button v-on:click="setClass">Alterar a classe</button>
+</template>
+
+<style scoped>
+.text-danger  {
+    color: #d32525;
+    font-weight:bold;
+    font-style: italic;
+}
+</style>
