8f4cdf211ded2a39507cb3b2ed368d966de1d0eb
AndersonCamposs <anderson.camposs87@outlook.com>
2024-11-17 11:55:08 -0300

    Manipulação de eventos

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import ReactiveData from "./components/ReactiveData.vue";
 import ClassBinding from "./components/ClassBinding.vue";
 import ConditionalRendering from "./components/ConditionalRendering.vue";
 import ListRendering from "./components/ListRendering.vue";
+import EventHandling from "./components/EventHandling.vue";
 </script>
 
 <template>
@@ -19,6 +20,8 @@ import ListRendering from "./components/ListRendering.vue";
   <ConditionalRendering />
   <hr>
   <ListRendering />
+  <hr>
+  <EventHandling />
 </template>
 
 <style scoped>
