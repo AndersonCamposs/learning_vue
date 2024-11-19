0a17d2732363e9ca212248b92b2691c47e4e0728
Anderson Campos <anderson.camposs87@outlook.com>
2024-11-19 20:18:50 -0300

    Componente customizado (com a utilização de props e emits)

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,15 @@ import ListRendering from "./components/ListRendering.vue";
 import EventHandling from "./components/EventHandling.vue";
 import Whatchers from "./components/Whatchers.vue";
 import CustomComponent from "./components/CustomComponent.vue";
+
+import { ref } from "vue";
+const textFontSize = ref(1);
+
+const books = ref([
+  {id: 1, title: "Dom Quixote", authorName: "Miguel de Cervantes"},
+  {id: 2, title: "Os Miseráveis", authorName: "Victor Hugo"},
+  {id: 3, title: "Meditações", authorName: "Marco Aurélio"},
+]);
 </script>
 
 <template>
@@ -28,7 +37,14 @@ import CustomComponent from "./components/CustomComponent.vue";
   <Whatchers />
   <hr>
   <h1>Componente customizado (props and emits)</h1>
-  <CustomComponent title="Dom Quixote" author-name="Miguel de Cervantes"/>
+  <CustomComponent 
+  v-bind:style="{fontSize: textFontSize + 'em'}"
+  @text-increment="textFontSize += 0.1"
+  @text-decrement="textFontSize -= 0.1"
+  v-for="book in books"
+  :key="book.id"
+  :title="book.title" 
+  :authorName="book.authorName"/>
 </template>
 
 <style scoped>
--- src/components/CustomComponent.vue
+++ src/components/CustomComponent.vue
@@ -3,12 +3,19 @@ defineProps([
     "title",
     "authorName"
 ])
+
+const emit = defineEmits([
+    "text-increment",
+    "text-decrement"
+])
 </script>
 
 <template>
     <div class="container">
         <p>Título: {{ title }}</p>
         <p>Nome do Autor: {{ authorName }}</p>
+        <button @click="() => emit('text-increment')">Aumentar fonte</button>
+        <button @click="() => emit('text-decrement')">Diminuir fonte</button>
     </div>
 </template>
 
@@ -17,5 +24,11 @@ defineProps([
     border: 2px solid #000;
     border-radius: 8px;
     text-align: center;
+    padding: 8px;
+    margin-bottom: 20px
+}
+
+button {
+    margin: 0 7px;
 }
 </style>
